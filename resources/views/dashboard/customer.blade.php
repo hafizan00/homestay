9cdfb3d61dd751c19f70d8bc343edbf7f1b8b0db
hafizan00 <hafizanabdila26@gmail.com>
2022-12-17 19:15:43 +0800

    @update:dashboard

--- resources/views/dashboard/customer.blade.php
+++ resources/views/dashboard/customer.blade.php
@@ -4,6 +4,28 @@
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 This is customer dashboard
             </div>
+
+            @foreach ($bookings as $booking)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('components.input-label', ['value' => $booking->id])
+                    </div>
+                    <div class="max-w-xl">
+                        @include('components.input-label', ['value' => $booking->name])
+                    </div>
+                    <div class="max-w-xl">
+                        @include('components.input-label', ['value' => $booking->email])
+                    </div>
+                    <div class="max-w-xl">
+                        @include('components.input-label', ['value' => $booking->created_at])
+                    </div>
+                    <div class="max-w-xl">
+                        @include('components.input-label', ['value' => $booking->updated_at])
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +20,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $bookings = User::all();
+    return view('dashboard', compact('bookings'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
